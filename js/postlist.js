430c04aa391fc578a38e9214d345af038bc710b9
McWenker <nlax76@gmail.com>
2016-11-09 18:41:27 -0500

    backlogged blog, postlist.js

---
+++ js/postlist.js
@@ -0,0 +1,30 @@
+document.getElementById("post-list").innerHTML =
+	'<ul class="posts">'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
+					'<h3><a href="../1109-appalachia">Appalachia, VA/NC/TN/AL</a></h3>'+
+						'<time class="published" datetime="2016-11-09">November 9, 2016</time>'+
+				'</header>'+
+				'<a href="../1109-appalachia" class="image"><img alt="" src="../../img/blog/11092016s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
+					'<h3><a href="../1028-on-the-road-again">On the Road Again, NY/NJ/PA</a></h3>'+
+						'<time class="published" datetime="2016-10-28">October 28, 2016</time>'+
+				'</header>'+
+				'<a href="../1028-on-the-road-again" class="image"><img alt="" src="../../img/blog/10282016s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
+					'<h3><a href="../1022-pitstop-becomes-detour">Pitstop Becomes Detour, MA</a></h3>'+
+						'<time class="published" datetime="2016-10-22">October 22, 2016</time>'+
+				'</header>'+
+				'<a href="../1022-pitstop-becomes-detour" class="image"><img alt="" src="../../img/blog/10222016s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+
+	'</ul>';
