789890643d7004d9f5fb3f3293a5562a52ee7d65
McWenker <nlax76@gmail.com>
2017-03-13 21:24:11 -0400

    the lost months

--- js/postlist.js
+++ js/postlist.js
@@ -3,28 +3,28 @@ document.getElementById("post-list").innerHTML =
 		'<li>'+
 			'<article>'+
 				'<header>'+
-					'<h3><a href="../1109-appalachia">Appalachia, VA/NC/TN/AL</a></h3>'+
-						'<time class="published" datetime="2016-11-09">November 9, 2016</time>'+
+					'<h3><a href="../0313-things-to-come">Things to Come, CA</a></h3>'+
+						'<time class="published" datetime="2017-03-13">March 13, 2017</time>'+
 				'</header>'+
-				'<a href="../1109-appalachia" class="image"><img alt="" src="../../img/blog/11092016s.jpg"/></a>'+
+				'<a href="../0313-things-to-come" class="image"><img alt="" src="../../img/blog/03132017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 		'<li>'+
 			'<article>'+
 				'<header>'+
-					'<h3><a href="../1028-on-the-road-again">On the Road Again, NY/NJ/PA</a></h3>'+
-						'<time class="published" datetime="2016-10-28">October 28, 2016</time>'+
+					'<h3><a href="../0309-omnislash">OMNISLASH</a></h3>'+
+						'<time class="published" datetime="2017-03-09">March 9, 2017</time>'+
 				'</header>'+
-				'<a href="../1028-on-the-road-again" class="image"><img alt="" src="../../img/blog/10282016s.jpg"/></a>'+
+				'<a href="../0309-omnislash" class="image"><img alt="" src="../../img/blog/03092017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 		'<li>'+
 			'<article>'+
 				'<header>'+
-					'<h3><a href="../1022-pitstop-becomes-detour">Pitstop Becomes Detour, MA</a></h3>'+
-						'<time class="published" datetime="2016-10-22">October 22, 2016</time>'+
+					'<h3><a href="../0301-the-lost-months">The Lost Months, FL/LA/MS/TX/NM/AZ/CA</a></h3>'+
+						'<time class="published" datetime="2017-03-01">March 1, 2017</time>'+
 				'</header>'+
-				'<a href="../1022-pitstop-becomes-detour" class="image"><img alt="" src="../../img/blog/10222016s.jpg"/></a>'+
+				'<a href="../0301-the-lost-months" class="image"><img alt="" src="../../img/blog/03012017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 	'</ul>';
