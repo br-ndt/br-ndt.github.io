643af83a89d802d464accbca397c5c8adc9ee881
McWenker <nlax76@gmail.com>
2017-03-22 22:03:13 -0400

    blog 0322

--- js/postlist.js
+++ js/postlist.js
@@ -3,10 +3,19 @@ document.getElementById("post-list").innerHTML =
 		'<li>'+
 			'<article>'+
 				'<header>'+
+					'<h3><a href="../0322-not-sequoia-but-redwood">Not Sequoia, But Redwood, CA</a></h3>'+
+						'<time class="published" datetime="2017-03-22">March 22, 2017</time>'+
+				'</header>'+
+				'<a href="../0313-things-to-come" class="image"><img alt="" src="../../img/blog/03222017s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
 					'<h3><a href="../0313-things-to-come">Things to Come, CA</a></h3>'+
 						'<time class="published" datetime="2017-03-13">March 13, 2017</time>'+
 				'</header>'+
-				'<a href="../0313-things-to-come" class="image"><img alt="" src="../../img/blog/03132017s.jpg"/></a>'+
+				'<a href="../0309-omnislash" class="image"><img alt="" src="../../img/blog/03132017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 		'<li>'+
@@ -15,16 +24,7 @@ document.getElementById("post-list").innerHTML =
 					'<h3><a href="../0309-omnislash">OMNISLASH</a></h3>'+
 						'<time class="published" datetime="2017-03-09">March 9, 2017</time>'+
 				'</header>'+
-				'<a href="../0309-omnislash" class="image"><img alt="" src="../../img/blog/03092017s.jpg"/></a>'+
-			'</article>'+
-		'</li>'+
-		'<li>'+
-			'<article>'+
-				'<header>'+
-					'<h3><a href="../0301-the-lost-months">The Lost Months, FL/LA/MS/TX/NM/AZ/CA</a></h3>'+
-						'<time class="published" datetime="2017-03-01">March 1, 2017</time>'+
-				'</header>'+
-				'<a href="../0301-the-lost-months" class="image"><img alt="" src="../../img/blog/03012017s.jpg"/></a>'+
+				'<a href="../0301-the-lost-months" class="image"><img alt="" src="../../img/blog/03092017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 	'</ul>';
