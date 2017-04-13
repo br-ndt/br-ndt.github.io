9e0d6ecc2834c000f9f374f467ed5243837f1d04
McWenker <nlax76@gmail.com>
2017-04-13 19:19:17 -0400

    0413 blogpost

--- js/postlist.js
+++ js/postlist.js
@@ -3,6 +3,15 @@ document.getElementById("post-list").innerHTML =
 		'<li>'+
 			'<article>'+
 				'<header>'+
+					'<h3><a href="../0413-washingtonia">Washingtonia, WA</a></h3>'+
+						'<time class="published" datetime="2017-04-13">April 13, 2017</time>'+
+				'</header>'+
+				'<a href="../0413-washingtonia" class="image"><img alt="" src="../../img/blog/04132017s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
 					'<h3><a href="../0404-beaver-evergreen">Beaver & Evergreen, OR/WA</a></h3>'+
 						'<time class="published" datetime="2017-04-04">April 4, 2017</time>'+
 				'</header>'+
@@ -16,15 +25,6 @@ document.getElementById("post-list").innerHTML =
 						'<time class="published" datetime="2017-03-22">March 22, 2017</time>'+
 				'</header>'+
 				'<a href="../0322-not-sequoia-but-redwood" class="image"><img alt="" src="../../img/blog/03222017s.jpg"/></a>'+
-			'</article>'+
-		'</li>'+
-		'<li>'+
-			'<article>'+
-				'<header>'+
-					'<h3><a href="../0313-things-to-come">Things to Come, CA</a></h3>'+
-						'<time class="published" datetime="2017-03-13">March 13, 2017</time>'+
-				'</header>'+
-				'<a href="../0313-things-to-come" class="image"><img alt="" src="../../img/blog/03132017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 	'</ul>';
