c824e20fd766065babf9dfb85b486a55003cfaa6
McWenker <nlax76@gmail.com>
2017-04-23 22:20:53 -0400

    0423 blogpost

--- js/postlist.js
+++ js/postlist.js
@@ -3,6 +3,15 @@ document.getElementById("post-list").innerHTML =
 		'<li>'+
 			'<article>'+
 				'<header>'+
+					'<h3><a href="../0423-oh-canada">Oh, Canada, BC</a></h3>'+
+						'<time class="published" datetime="2017-04-23">April 23, 2017</time>'+
+				'</header>'+
+				'<a href="../0423-oh-canada" class="image"><img alt="" src="../../img/blog/04232017s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
 					'<h3><a href="../0413-washingtonia">Washingtonia, WA</a></h3>'+
 						'<time class="published" datetime="2017-04-13">April 13, 2017</time>'+
 				'</header>'+
@@ -16,15 +25,6 @@ document.getElementById("post-list").innerHTML =
 						'<time class="published" datetime="2017-04-04">April 4, 2017</time>'+
 				'</header>'+
 				'<a href="../0404-beaver-evergreen" class="image"><img alt="" src="../../img/blog/04042017s.jpg"/></a>'+
-			'</article>'+
-		'</li>'+
-		'<li>'+
-			'<article>'+
-				'<header>'+
-					'<h3><a href="../0322-not-sequoia-but-redwood">Not Sequoia, But Redwood, CA</a></h3>'+
-						'<time class="published" datetime="2017-03-22">March 22, 2017</time>'+
-				'</header>'+
-				'<a href="../0322-not-sequoia-but-redwood" class="image"><img alt="" src="../../img/blog/03222017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 	'</ul>';
