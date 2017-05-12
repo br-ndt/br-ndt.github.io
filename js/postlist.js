566b056955bc88bd4b2590519ae05daf98b85e01
McWenker <nlax76@gmail.com>
2017-05-11 22:19:14 -0400

    blog 0511, writing updates

--- js/postlist.js
+++ js/postlist.js
@@ -3,28 +3,28 @@ document.getElementById("post-list").innerHTML =
 		'<li>'+
 			'<article>'+
 				'<header>'+
+					'<h3><a href="../0511-yellowstone">Yellowstone, MT/WY/ID</a></h3>'+
+						'<time class="published" datetime="2017-05-11">May 11, 2017</time>'+
+				'</header>'+
+				'<a href="../0511-yellowstone" class="image"><img alt="0511 image" src="../../img/blog/05112017s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
+					'<h3><a href="../hotsprings">Hot Springs, WA/BC/AB</a></h3>'+
+						'<time class="published" datetime="2017-04-30">April 30, 2017</time>'+
+				'</header>'+
+				'<a href="../hotsprings" class="image"><img alt="0430 image" src="../../img/blog/04302017s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
 					'<h3><a href="../0423-oh-canada">Oh, Canada, BC</a></h3>'+
 						'<time class="published" datetime="2017-04-23">April 23, 2017</time>'+
 				'</header>'+
-				'<a href="../0423-oh-canada" class="image"><img alt="" src="../../img/blog/04232017s.jpg"/></a>'+
-			'</article>'+
-		'</li>'+
-		'<li>'+
-			'<article>'+
-				'<header>'+
-					'<h3><a href="../0413-washingtonia">Washingtonia, WA</a></h3>'+
-						'<time class="published" datetime="2017-04-13">April 13, 2017</time>'+
-				'</header>'+
-				'<a href="../0413-washingtonia" class="image"><img alt="" src="../../img/blog/04132017s.jpg"/></a>'+
-			'</article>'+
-		'</li>'+
-		'<li>'+
-			'<article>'+
-				'<header>'+
-					'<h3><a href="../0404-beaver-evergreen">Beaver & Evergreen, OR/WA</a></h3>'+
-						'<time class="published" datetime="2017-04-04">April 4, 2017</time>'+
-				'</header>'+
-				'<a href="../0404-beaver-evergreen" class="image"><img alt="" src="../../img/blog/04042017s.jpg"/></a>'+
+				'<a href="../0423-oh-canada" class="image"><img alt="0423 image" src="../../img/blog/04232017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 	'</ul>';
