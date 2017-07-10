1e6f163dbc84f3982c7fbc3eedc22093d3641421
McWenker <nlax76@gmail.com>
2017-07-09 21:19:13 -0400

    0709 blog

--- js/postlist.js
+++ js/postlist.js
@@ -1,5 +1,14 @@
 document.getElementById("post-list").innerHTML =
-	'<ul class="posts">'+		
+	'<ul class="posts">'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
+					'<h3><a href="../07092017">Across the Desert(s) ii, UT/CO</a></h3>'+
+						'<time class="published" datetime="2017-07-09">July 9, 2017</time>'+
+				'</header>'+
+				'<a href="../07092017" class="image"><img alt="0709 image" src="../../img/blog/07092017s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+		
 		'<li>'+
 			'<article>'+
 				'<header>'+
@@ -16,15 +25,6 @@ document.getElementById("post-list").innerHTML =
 						'<time class="published" datetime="2017-06-08">June 8, 2017</time>'+
 				'</header>'+
 				'<a href="../06082017" class="image"><img alt="06082017 image" src="../../img/blog/06082017s.jpg"/></a>'+
-			'</article>'+
-		'</li>'+
-		'<li>'+
-			'<article>'+
-				'<header>'+
-					'<h3><a href="../05112017">Yellowstone, MT/WY/ID</a></h3>'+
-						'<time class="published" datetime="2017-05-11">May 11, 2017</time>'+
-				'</header>'+
-				'<a href="../05112017" class="image"><img alt="05112017 image" src="../../img/blog/05112017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 	'</ul>';
