2c2bc26e798f6c76dbda2fab97bc60dfaab3d66c
McWenker <nlax76@gmail.com>
2017-08-03 19:54:50 -0400

    08032017 blog

--- js/postlist.js
+++ js/postlist.js
@@ -3,6 +3,15 @@ document.getElementById("post-list").innerHTML =
 		'<li>'+
 			'<article>'+
 				'<header>'+
+					'<h3><a href="../08032017">Rocky Mountains, Black Hills, CO/WY/SD</a></h3>'+
+						'<time class="published" datetime="2017-08-03">August 3, 2017</time>'+
+				'</header>'+
+				'<a href="../08032017" class="image"><img alt="08032017 image" src="../../img/blog/08032017s.jpg"/></a>'+
+			'</article>'+
+		'</li>'+
+		'<li>'+
+			'<article>'+
+				'<header>'+
 					'<h3><a href="../07092017">Across the Desert(s) ii, UT/CO</a></h3>'+
 						'<time class="published" datetime="2017-07-09">July 9, 2017</time>'+
 				'</header>'+
@@ -16,15 +25,6 @@ document.getElementById("post-list").innerHTML =
 						'<time class="published" datetime="2017-06-26">June 26, 2017</time>'+
 				'</header>'+
 				'<a href="../06262017" class="image"><img alt="06262017 image" src="../../img/blog/06262017s.jpg"/></a>'+
-			'</article>'+
-		'</li>'+
-		'<li>'+
-			'<article>'+
-				'<header>'+
-					'<h3><a href="../06082017">West Coast Redux, OR/NV/CA</a></h3>'+
-						'<time class="published" datetime="2017-06-08">June 8, 2017</time>'+
-				'</header>'+
-				'<a href="../06082017" class="image"><img alt="06082017 image" src="../../img/blog/06082017s.jpg"/></a>'+
 			'</article>'+
 		'</li>'+
 	'</ul>';
