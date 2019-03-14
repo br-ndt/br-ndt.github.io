25504bad4672373abd6f17bb5217d516e8cb73a5
McWenker <nlax76@gmail.com>
2019-03-13 23:34:09 -0400

    potentially destroying everything
    potential jekyll update, or maybe just killing my site

---
+++ assets/javascript/about-tabs.js
@@ -0,0 +1,19 @@
+function openAbout(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks, tabisopen;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("about-tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("about-tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
