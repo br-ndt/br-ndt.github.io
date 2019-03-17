c66f7e1f41b6fed4395082ad05601bd8ffc6ee9a
McWenker <nlax76@gmail.com>
2019-03-17 00:30:01 -0400

    folk heroes page
    added some character info, with much more to come

--- assets/javascript/about-tabs.js
+++ assets/javascript/about-tabs.js
@@ -3,13 +3,13 @@ function openAbout(evt, tabName) {
   var i, tabcontent, tablinks, tabisopen;
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("about-tabcontent");
+  tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("about-tablinks");
+  tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
